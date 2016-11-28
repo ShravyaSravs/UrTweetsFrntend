d73e9fc6658ec5c7218cb2c0c37824c33545aae8
shravya <shravya@LAPTOP-RB2O83EE>
2016-11-28 16:22:43 +0530

    collab front end

---
+++ WebContent/Blog/blogctrl.js
@@ -0,0 +1,138 @@
+app.controller('blogctrl', [ '$scope', '$http', function($scope, $http) {
+	$scope.userid=document.getElementById("userid").value; 
+	$scope.submit = function() {
+		var BASE_URL = 'http://localhost:13026/CollabFrontEnd';
+		$scope.blog = {	
+			bid      : $scope.blogid,
+			blogName : $scope.blogName,
+			blogDesc : $scope.blogDesc,
+		}
+		$http({
+			method : 'POST',
+			url : BASE_URL + '/CreateBlog',
+			data : $scope.blog
+		}).success(function(data, status, headers, config) {
+			//alert("Success");
+			$scope.blogid ='';
+			$scope.blogName = '';
+			$scope.blogDesc = '';
+			$scope.getAllBlogs();
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});	
+	};
+	
+	
+	$scope.getAllBlogs= function() {
+		$http({
+			method : 'GET',
+			url : 'getAllBlogs'
+		}).success(function(data, status, headers, config) {
+			$scope.blogs = data;
+			//alert(data); 
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	
+	
+	$scope.deleteBlog = function(bid) {
+		$http({
+			method : 'DELETE',
+			url : 'deleteBlog/'+bid
+		}).success(function(data, status, headers, config) {
+			//alert("Success"); 
+			$scope.getAllBlogs();
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	}
+	
+	
+	$scope.editBlog = function(bid) {
+		$http({
+			method : 'GET',
+			url : 'editBlog/'+bid
+		}).success(function(data, status, headers, config) {
+			$scope.blog = data;// alert(data); 
+			$scope.blogid = $scope.blog.bid;
+			$scope.blogName = $scope.blog.blogName;
+			$scope.blogDesc = $scope.blog.blogDesc;
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	
+	$scope.accessBlog = function(b_userid) {
+		//alert("Hello"+b_userid);
+		if($scope.userid==b_userid) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	$scope.getAllBlogs();
+}]);
+
+
+
+
+
+
+/*var app = angular.module('myApp',[]);
+app.controller('blogcntrl', [ '$scope', '$http', function($scope, $http) {
+	var BASE_URL = 'http://localhost:8036/CollabFrontEnd';
+
+	$scope.getAllBlogs= function() {
+		console.log("get all blogs")
+		$http({
+			method : 'GET',
+			url : BASE_URL+'/blog'
+		}).success(function(data, status, headers, config) {
+			$scope.blogs=data;
+			//alert(data); 
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	$scope.submit = function() {
+		console.log("create blog")
+		
+		$scope.blog = {	
+			id:$scope.id,
+			title : $scope.title,
+			userid:$scope.userid,
+			doc:$scope.doc,
+			content : $scope.content,
+		}
+		$http({
+			method : 'POST',
+			url : BASE_URL + '/createblog',
+			data : $scope.blog
+		}).success(function(data, status, headers, config) {
+			$scope.id='';
+			$scope.title='';
+			$scope.userid='';
+			$scope.doc='';
+			$scope.content='';
+			$scope.getAllBlogs();
+		}).error(function(data,status,headers,config){
+			alert("error");
+		});
+	};
+	$scope.deleteblog=function(id){
+		$http({
+			method:'DELETE',
+		url:BASE_URL+'/deleteblog/'+id
+		}).success(function(data,status,headers,config){
+			$scope.getAllBlogs();
+		})
+	};
+	$scope.editblog=function(id,title,content){
+		$scope.id=id;
+		$scope.title=title;
+		$scope.content=content;
+	}
+	        }]);
+*/*/
---
+++ WebContent/Forum/forumctrl.js
@@ -0,0 +1,138 @@
+app.controller('forumctrl', [ '$scope', '$http', function($scope, $http) {
+	  $scope.userid=document.getElementById("userid").value; 
+	  var BASE_URL = 'http://localhost:13026/CollabFrontEnd';
+	  $scope.submit = function() {	
+		$scope.forum = {	
+			fid       : $scope.forumid,
+			forumName : $scope.forumName,
+			forumDesc : $scope.forumDesc,
+		}
+		$http({
+			method : 'POST',
+			url : BASE_URL +'/CreateForum',
+			data : $scope.forum
+		}).success(function(data, status, headers, config) {
+			//alert("Success");
+			$scope.forumid ='';
+			$scope.forumName = '';
+			$scope.forumDesc = '';
+			$scope.getAllForums();
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});	
+	};
+	
+	
+	$scope.getAllForums = function() {
+		$http({
+			method : 'GET',
+			url : 'getAllForums'
+		}).success(function(data, status, headers, config) {
+			$scope.forums = data;// alert(data); 
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	
+	$scope.getSingleForum = function() {
+		$http({
+			method : 'GET',
+			url : 'Forum'
+		}).success(function(data, status, headers, config) {
+			$scope.forums = data;// alert(data); 
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	
+	$scope.deleteForum = function(fid) {
+		$http({
+			method : 'DELETE',
+			url : 'deleteForum/'+fid
+		}).success(function(data, status, headers, config) {
+			//alert("Success"); 
+			$scope.getAllForums();
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	}
+	
+
+	$scope.editForum = function(fid) {
+		$http({
+			method : 'GET',
+			url : 'editForum/'+fid
+		}).success(function(data, status, headers, config) {
+			$scope.forum = data;// alert(data); 
+			$scope.forumid = $scope.forum.fid;
+			$scope.forumName = $scope.forum.forumName;
+			$scope.forumDesc = $scope.forum.forumDesc;
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	
+	$scope.accessForum = function(f_userid) {
+		if($scope.userid==f_userid)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	$scope.getAllForums();
+}]);
+
+
+
+
+
+
+/*var app = angular.module('forumApp',[]);
+app.controller('forumctrl', [ '$scope', '$http', function($scope, $http) {
+	var BASE_URL = 'http://localhost:8036/CollabFrontEnd';
+	$scope.getAllForum= function() {
+		console.log("get all forum")
+		$http({
+			method : 'GET',
+			url : BASE_URL+'/forum'
+		}).success(function(data, status, headers, config) {
+			$scope.forums=data;
+			//alert(data); 
+		}).error(function(data, status, headers, config) {
+			alert("Error");
+		});
+	};
+	$scope.submit = function() {
+		$scope.forum = {	
+			name : $scope.name,
+			topic:$scope.topic,
+			userid:$scope.userid,
+			doc: $scope.doc,
+			description:$scope.description
+		}
+		$http({
+			method : 'POST',
+			url : BASE_URL + '/createforum',
+			data : $scope.forum
+		}).success(function(data, status, headers, config) {
+			$scope.name='';
+			$scope.topic='';
+			$scope.userid='';
+			$scope.doc='';
+			$scope.description='';
+		}).error(function(data,status,headers,config){
+			alert("error");
+		});
+	};
+	$scope.deleteforum=function(id){
+		$http({
+			method:'DELETE',
+		url:BASE_URL+'/deleteforum/'+id
+		}).success(function(data,status,headers,config){
+			$scope.getAllForum();
+		})
+	};
+}]);*/
